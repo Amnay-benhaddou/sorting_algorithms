a39948065866d37f71267de19dacf4886d2ab1b8
amnaybenhaddou <amnaybenhaddou98@gmail.com>
2024-01-27 07:32:46 -0800

    Big O notatio

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort a elements from min to max value
+ * @a: a
+ * @size: a size
+ */
+void bubble_sort(int *a, size_t size)
+{
+size_t i, j, tmp = 0;
+if (size < 2)
+{
+return;
+}
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (a[j] > a[j + 1] && a[j + 1)
+{
+tmp = a[j];
+a[j] = a[j + 1];
+a[j + 1] = tmp;
+print_array(a, size);
+}
+}
+}
+}
