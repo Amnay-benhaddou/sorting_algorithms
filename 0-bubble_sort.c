65478ea252fd3da01d4030c92bf6bff196070457
amnaybenhaddou <amnaybenhaddou98@gmail.com>
2024-01-27 10:29:46 -0800

    big o notation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,36 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array elements from min to max value
- * @array: array
- * @size: array size
- */
+* bubble_sort - sorts an array of integers in ascending order
+* using the Bubble sort algorithm
+* @array: Array to sort
+* @size: size of the array
+*
+* Return: Void
+*/
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j, tmp = 0;
-if (size < 2)
-{
+size_t i, j, size2 = size;
+int tmp, k = 0;
+
+if (array == NULL || size < 2)
 return;
-}
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++)
+for (j = 1; j < size2; j++)
 {
-if (array[j] > array[j + 1] && array[j + 1])
+if (array[j - 1] > array[j])
 {
+k = 1;
 tmp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = tmp;
+array[j] = array[j - 1];
+array[j - 1] = tmp;
 print_array(array, size);
 }
 }
+if (k == 0)
+break;
+k = 0;
+size2--;
 }
 }
