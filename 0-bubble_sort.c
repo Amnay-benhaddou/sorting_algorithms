07e6406e7845da69ebaf763ef35895fcdff27601
amnaybenhaddou <amnaybenhaddou98@gmail.com>
2024-01-27 07:45:50 -0800

    big notation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 /**
- * bubble_sort - sort a elements from min to max value
- * @a: a
- * @size: a size
+ * bubble_sort - sort array elements from min to max value
+ * @array: array
+ * @size: array size
  */
-void bubble_sort(int *a, size_t size)
+void bubble_sort(int *array, size_t size)
 {
 size_t i, j, tmp = 0;
 if (size < 2)
@@ -15,12 +15,12 @@ for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
-if (a[j] > a[j + 1] && a[j + 1])
+if (array[j] > array[j + 1] && array[j + 1])
 {
-tmp = a[j];
-a[j] = a[j + 1];
-a[j + 1] = tmp;
-print_array(a, size);
+tmp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = tmp;
+print_array(array, size);
 }
 }
 }
